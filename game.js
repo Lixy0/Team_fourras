7af4e473257c8c61f7d15a6d6286a13a285dcc07
AndreasVanmeerbeck <39094834+Ashfurr@users.noreply.github.com>
2022-03-14 16:03:27 +0100

    oui
    oui

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 const config = {
   type: Phaser.AUTO,
   parent: 'game',
-  width: 800,
+  width: 1250,
   heigth: 640,
   scale: {
     mode: Phaser.Scale.RESIZE,
@@ -23,9 +23,12 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+
 function preload() {
   this.load.image('background', 'assets/images/background.png');
   this.load.image('spike', 'assets/images/spike.png');
+  this.load.image('move', 'assets/images/mouvable.png');
+  this.load.image('save', 'assets/images/Save.png');
   // At last image must be loaded with its JSON
   this.load.atlas('player', 'assets/images/kenney_player.png','assets/images/kenney_player_atlas.json');
   this.load.image('tiles', 'assets/tilesets/platformPack_tilesheet.png');
@@ -33,14 +36,23 @@ function preload() {
   this.load.tilemapTiledJSON('map', 'assets/tilemaps/level1.json');
 }
 
+
+
 function create() {
+  /**
+   * déclaration des variables
+   * @type {number}
+   */
+  this.currentSaveX=1
+  this.currentSaveY=1
+
   /**
    * creation decor
    *
    */
-  const backgroundImage = this.add.image(0, 0,'background').setOrigin(0, 0);
+  const backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0);
   backgroundImage.setScale(2, 0.8);
-  const map = this.make.tilemap({ key: 'map' });
+  const map = this.make.tilemap({key: 'map'});
   const tileset = map.addTilesetImage('kenny_simple_platformer', 'tiles');
   const platforms = map.createStaticLayer('Platforms', tileset, 0, 200);
   platforms.setCollisionByExclusion(-1, true);
@@ -50,7 +62,6 @@ function create() {
    */
   this.player = this.physics.add.sprite(50, 300, 'player');
   this.player.setBounce(0.1);
-  this.player.setCollideWorldBounds(true);
   this.physics.add.collider(this.player, platforms);
   /**
    *
@@ -67,12 +78,12 @@ function create() {
   });
   this.anims.create({
     key: 'idle',
-    frames: [{ key: 'player', frame: 'robo_player_0' }],
+    frames: [{key: 'player', frame: 'robo_player_0'}],
     frameRate: 10,
   });
   this.anims.create({
     key: 'jump',
-    frames: [{ key: 'player', frame: 'robo_player_1' }],
+    frames: [{key: 'player', frame: 'robo_player_1'}],
     frameRate: 10,
   });
   this.cursors = this.input.keyboard.createCursorKeys();
@@ -82,12 +93,11 @@ function create() {
    */
   // Create a sprite group for all spikes, set common properties to ensure that
 // sprites in the group don't move via gravity or by player collisions
-  this.Save= this.physics.add.group({allowGravity:false,immovable:true})
+
   this.spikes = this.physics.add.group({
     allowGravity: false,
     immovable: true
   });
-  map.getObjectLayer('Save').objects.forEach((Save) => {})
 
 // Let's get the spike objects, these are NOT sprites
 // We'll create spikes in our sprite group for each object in our map
@@ -96,11 +106,59 @@ function create() {
     spikeSprite.body.setSize(spike.width, spike.height - 20).setOffset(0, 20);
   });
   this.physics.add.collider(this.player, this.spikes, playerHit, null, this);
+
+  this.saves = this.physics.add.group({
+    allowGravity: false,
+    immovable: true
+  });
+
+// Let's get the save objects, these are NOT sprites
+// We'll create saves in our sprite group for each object in our map
+  map.getObjectLayer('Save').objects.forEach((save) => {
+    const saveSprite = this.saves.create(save.x, save.y + 200 - save.height, 'save').setOrigin(0);
+  });
+  this.physics.add.overlap(this.player, this.saves, sauvegarde,null, this)
+
+  this.moves = this.physics.add.group({
+    allowGravity: false,
+    immovable: false
+  });
+
+// Let's get the move objects, these are NOT sprites
+// We'll create moves in our sprite group for each object in our map
+
+  let trucsBleu=[];
+
+  let nom=0;
+  map.getObjectLayer('Mouvable').objects.forEach((move) => {
+    const moveSprite = this.moves.create(move.x, move.y + 200 - move.height, 'move').setOrigin(0);
+    moveSprite.name=nom++
+    trucsBleu.push(moveSprite);
+  });
+  this.physics.add.collider(this.player, this.moves, pousser,null, this)
+  this.physics.add.collider(this.moves,this.moves)
+  this.cameras.main.startFollow(this.player);
 }
+function  sauvegarde (player, saves){
+  console.log("current",this.currentSaveX,this.currentSaveY)
+  this.currentSaveX = player.x
+  this.currentSaveY = player.y
+  saves.body.enable=false;
+}
+function pousser(player,moves){
+  if (player.body.touching.right && moves.body.touching.left){
+    moves.x+=1
+  }
+  if (player.body.touching.left && moves.body.touching.right){
+    moves.x-=1
+  }
+
+}
+
 function playerHit(player, spike) {
   player.setVelocity(0, 0);
-  player.setX(50);
-  player.setY(300);
+  player.x=this.currentSaveX
+  player.y=this.currentSaveY;
   player.play('idle', true);
   player.setAlpha(0);
   let tw = this.tweens.add({
@@ -113,6 +171,8 @@ function playerHit(player, spike) {
 }
 
 function update() {
+
+
   // Control the player with left or right keys
   if (this.cursors.left.isDown) {
     this.player.setVelocityX(-200);
